e96421eeb4482bdaedd5bce2c28ed931382cb5be
Raj Mishra <rajrm121212@gmail.com>
2024-08-26 07:22:19 +0000

    array all methods done

--- .devcontainer/JS_Setup/Array.js
+++ .devcontainer/JS_Setup/Array.js
@@ -18,13 +18,28 @@ const supes = ["Homelander","Starlight","Deep"];
 // console.log(myArr);
 
 // Difference b/w Slice and Splice
-const m1 = myArr.slice(1,4);
-console.log(m1);
-console.log(myArr);
+// const m1 = myArr.slice(1,4);
+// console.log(m1);
+// console.log(myArr);
 
-const m2 = myArr.splice(1,4);
-console.log(m2);
-console.log(myArr);
+// const m2 = myArr.splice(1,4);
+// console.log(m2);
+// console.log(myArr);
+
+//concat
+console.log(myArr.concat(supes));
+
+//spread
+console.log([...myArr,...supes]);
+
+//flat
+const boys = ["hughie","butcher","mm","frenchie",["A-train","Starlight",["fbi","madam"]]];
+const fighters = boys.flat(Infinity);
+console.log(fighters);
+
+//isarray , array.from  , array.of
+console.log(Array.from("raj"));
+console.log(Array.of("raj",3,7,8));
 
 
 
