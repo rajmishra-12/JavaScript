8754ce6027e2a58e87fa63bb9e6b6a965445fbe0
Raj Mishra <rajrm121212@gmail.com>
2024-08-26 06:41:33 +0000

    Array methods

---
+++ .devcontainer/JS_Setup/Array.js
@@ -0,0 +1,38 @@
+//array
+const myArr = [1,2,3,4,5];
+const supes = ["Homelander","Starlight","Deep"];
+// console.log(myArr);
+
+//array methods
+
+// myArr.push(7);
+// supes.push("Stormfront");
+// console.log(supes);
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(0);
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+// Difference b/w Slice and Splice
+const m1 = myArr.slice(1,4);
+console.log(m1);
+console.log(myArr);
+
+const m2 = myArr.splice(1,4);
+console.log(m2);
+console.log(myArr);
+
+
+
+
+
+
+
+
+
+
+
